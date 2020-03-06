77d946f696485d6db140a9b04b6caf9c2c45e6e9
the-devin-leigh <dkhleigh@gmail.com>
2020-03-05 22:56:47 -0800

    working functions

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void makeHeap(int array[], int n, int i){ //heap array, n = size of heap, i = max index
   int max = i;
   int left = i*2 + 1; //if max index = 0, left and right must add one more
-  int right = i*2 +2;
+  int right = i*2 + 2;
 
   if(left < n && array[left] > array[max]) //if left greater than max, make left max
     max = left;
@@ -30,17 +30,18 @@ void sort(int array[], int n){
 }
 
 void printHeap(int array[], int n){ //print out heap w/ double the int per line
-  for(int i = 0; i < n; ++i){
-    for(int j = i; j <= pow(2,i); j++){
-      cout << array[i] << " ";
-    }
-    cout << endl;
+  //cout<<n<<endl;
+  for(int i = 0; i < n; i++){
+    cout << array[n-(i+1)] << " ";
+    /*for(int j = pow(2,i-1); j <= pow(2,i); j++){
+      cout << array[j-1] << " ";
+      }
+      cout << endl;*/
   }
 }
 
 int main(){
   char* input = new char[10];
-  int harray[10];
 
   cout << "File or Manual input?" << endl;
   cin.get(input, 10);
@@ -49,14 +50,22 @@ int main(){
 
   if(strcmp(input, "file") == 0){
     int i = 0;
+    char* fileCount = new char[10];
     char* filepath = new char[200];
     cout << "Enter file name: ";
     cin.get(filepath, 200);
     cin.clear();
     cin.ignore(100, '\n');
+
+    cout << "How many integers are in the file?" << endl;
+    cin >> fileCount;
+    cin.clear();
+    cin.ignore(100, '\n');
+    int harray[atoi(fileCount)];
+    
     ifstream File;
     File.open(filepath);
-
+    
     while(!File.eof()){
       File >> harray[i];
       i++;
@@ -64,7 +73,7 @@ int main(){
 
     File.close();
 
-    int n = sizeof(harray)/sizeof(harray[n]);
+    int n = sizeof(harray)/sizeof(harray[0]);
     sort(harray, n);
     cout << "Sorted." << endl;
     printHeap(harray, n);
@@ -73,22 +82,27 @@ int main(){
   if(strcmp(input, "manual") == 0){
     int i = 0;
     int lol = 0;
-    char* exit;
-    while(lol != 27){
-      int num;
+    char* exitCon = new char[10];
+    cout << "How many numbers will you enter? " << endl;
+    cin >> exitCon;
+    cin.clear();
+    cin.ignore(100, '\n');
+    int harray[atoi(exitCon)];
+    while(lol != atoi(exitCon)){
+      char* num = new char[10];
       cout << "Enter a number between 1 and 1000: ";
       cin >> num;
-      num = harray[i];
-      cout << "Are you finished entering?(y/n)" << endl;
-      cin >> exit;
-      if(*exit == 'y'){
-	lol = 27;
-      }else if(*exit == 'n'){
-	lol = 0;
-      }
+      cin.clear();
+      cin.ignore(100, '\n');
+      harray[i] = atoi(num);
+      i++;
+      lol++;
     }
 
+    
+
     int n = sizeof(harray)/sizeof(harray[0]);
+    
     sort(harray, n);
     cout << "Sorted." << endl;
     printHeap(harray, n);
